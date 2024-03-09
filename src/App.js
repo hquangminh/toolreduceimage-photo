09e2a1f6513c974e4f89e2c5f77f549096ef36ae
hquangminh <charleshuynh.connect@gmail.com>
2024-03-09 16:27:34 +0700

    add imahe reduce

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@
 import React from 'react'
 import './App.css'
 import UploadComponent from './CompressionToolImage/UploadComponent'
-import UploadComponentVideo from './CompressionToolVideo/UploadComponentVideo'
 
 function App() {
   return (
     <div className='App'>
       <UploadComponent />
-      {/* <UploadComponentVideo /> */}
     </div>
   )
 }
